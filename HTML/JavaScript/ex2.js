ddb5fcc5a6d85fc1e1cf328b77da67d21c52ca79
Bytes4Future <bytes4future@AKF21-03387.local>
2023-08-01 12:16:03 +0100

    oi3

--- HTML/JavaScript/ex2.js
+++ HTML/JavaScript/ex2.js
@@ -2,14 +2,12 @@ function minMax(arr, n)
 {
 	let arrLength = arr.length;
 	let sortedArr = arr.slice(0, arrLength).sort();
-	let maxSum = 0;
-	let minSum = 0;
+	let answer = [0,0];
 	for (i = 0; i < n; i++)
 	{
-		minSum += sortedArr[i];
-		maxSum += sortedArr[arrLength - i - 1];
+		answer[0] += sortedArr[i];
+		answer[1] += sortedArr[arrLength - i - 1];
 	}
-	let answer = [minSum, maxSum];
 	return answer;
 }
 console.log(minMax([7, 1, 1, 3, 2, 6], 3));
