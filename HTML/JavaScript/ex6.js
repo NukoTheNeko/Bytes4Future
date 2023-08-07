bad2e237ff64e92662913198d9877226531a5089
Bytes4Future <bytes4future@AKF21-03387.local>
2023-08-07 11:10:32 +0100

    Ex

--- HTML/JavaScript/ex6.js
+++ HTML/JavaScript/ex6.js
@@ -29,3 +29,57 @@ console.log(
 	mapToDouble([1, 1, 2]), //[2, 2, 4]
 	mapToDouble([]) //[]
 )
+
+//Ex 3
+
+console.log("Ex 3");
+function keepBeginningWithE (array)
+{
+	let newArray = new Array();
+	for(let word of array)
+	{
+		if(word.toLowerCase().startsWith("e"))
+		{
+			newArray.push(word);
+		}
+	}
+	return newArray;
+}
+console.log(
+	keepBeginningWithE(['Esposende', 'Algarve', 'Sintra']),
+	// ["Esposende"]
+  
+	keepBeginningWithE(['easfwe', 'asab', 'iasd', 'easn'])
+	// ["easfwe", "easn"]
+)
+
+//Ex 4
+
+console.log("Ex 4");
+function keepBeginningWithLetter(array, letter)
+{
+	let newArray = new Array();
+	for(let word of array)
+	{
+		if(word.toLowerCase().startsWith(letter))
+		{
+			newArray.push(word);
+		}
+	}
+	return newArray;
+}
+console.log(
+	keepBeginningWithLetter(['raposa', 'mesa'], 'm'),
+	// ["mesa"]
+	keepBeginningWithLetter(['dog', 'rat', "dossier"], "d")
+	// ["dog", "dossier"]
+)
+
+//Ex 5
+
+console.log("Ex 5");
+function getLastN(array,n)
+{
+	return array.slice(array.length - n, array.length);
+}
+console.log(getLastN(["a", "b", "c", "a", "e", "b"],3));
