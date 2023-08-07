86235f0dd9b72b41877400ff8d5e1022a1a276b1
NukoTheNeko <36847623+NukoTheNeko@users.noreply.github.com>
2023-08-08 00:33:52 +0100

    oi

--- HTML/JavaScript/index.js
+++ HTML/JavaScript/index.js
@@ -16,6 +16,7 @@ function Fibonacci(iter)
 	}
 	return Fibonacci(iter - 1) + Fibonacci(iter - 2);
 }
+	process.stdout.write(Fibonacci(15) + "");
 
 for (let i = 1; i <= 10; i++) 
 {
---
+++ HTML/JavaScript/smth.js
@@ -0,0 +1,4 @@
+function ordenaVelasAcesasPrimeiro(velas) {
+    return velas.sort().reverse()
+}
+console.log(ordenaVelasAcesasPrimeiro([true,true,false,true]));
