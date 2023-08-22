92e33c312aef1acb6df3d9ecf3d032485d676fa6
NukoTheNeko <36847623+NukoTheNeko@users.noreply.github.com>
2023-08-22 16:40:38 +0100

    lol

--- HTML/Revisions/final.js
+++ HTML/Revisions/final.js
@@ -75,3 +75,93 @@ console.log(findWord([
 	"OLO",
 	"OYO"
   ], "OLA"))
+  
+//Ex 4
+console.log("Ex 4");
+function ContainsSubMatrix(matrix, submatrix)
+{
+	let subLength =  submatrix.length;
+	let subDepth =  submatrix[0].length;
+	for(let i = 0; i <= matrix.length - subLength; i++)
+	{
+		for(let j = 0; j <= matrix[i].length - subDepth; j++)
+		{
+			if(matrix[i][j] == submatrix[0][0])
+			{
+				let confirmationArray = new Array(subLength);
+				for(let k = 0; k < subLength; k++)
+				{
+					confirmationArray[k] = matrix[i + k].slice(j, j + subDepth)
+				}
+				if(CompareArrays(submatrix,confirmationArray))
+				{
+					return true;
+				}
+			}
+		}
+	}
+	return false
+}
+function CompareArrays(array1, array2)
+{
+	for(let index1 = 0; index1 < array1.length; index1++)
+	{
+		for(let index2 = 0; index2 < array1[index1].length; index2++)
+		{
+			if(array1[index1][index2] !== array2[index1][index2])
+			{
+				return false;
+			}
+		}
+	}
+	return true;
+}
+console.log(ContainsSubMatrix([
+	[1, 0, 0, 1, 0],
+	[2, 3, 1, 4, 5],
+	[1, 1, 2, 4, 3],
+	[1, 2, 3, 4, 5]
+  ], [
+	[4, 5],
+	[4, 5]
+  ]))
+
+//Ex Final
+console.log("Ex Final");
+function Mathify(string)
+{
+	let inputs = string.split(" ");
+	let result;
+
+	let i = inputs.length - 1;
+	while(inputs.length > 1)
+	{
+		if(isNaN(Number(inputs[i])))
+		{
+			switch (inputs[i])
+			{
+				case "+":
+					result = Number(inputs[i + 1]) + Number(inputs[i + 2]);
+					break;
+				case "-":
+					result = Number(inputs[i + 1]) - Number(inputs[i + 2]);
+					break;
+				case "/":
+					result = Number(inputs[i + 1]) / Number(inputs[i + 2]);
+					break;
+				case "*":
+					result = Number(inputs[i + 1]) * Number(inputs[i + 2]);
+					break;
+			}
+			inputs.splice(i, 3, result);
+			i = inputs.length;
+		}
+		i--;
+	}
+	return inputs[0];
+}
+console.log(Mathify("0")); // 0
+console.log(Mathify("+ 3 4")); // 7
+console.log(Mathify("- 3 * 4 5")); // -17
+console.log(Mathify("* + 3 4 5")); // 35
+console.log(Mathify("/ - 3 4 + 5 2")); // 
